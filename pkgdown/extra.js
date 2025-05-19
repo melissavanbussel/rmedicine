bfbdaa9d86e0b0dddb9416971961b6235208d600
Melissa Van Bussel <melissalisevb@gmail.com>
2025-05-19 12:12:18 -0400

    Make JS button work properly

--- pkgdown/extra.js
+++ pkgdown/extra.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", function() {
   let javascriptAlert = document.querySelectorAll(".nav-link[href='#js_alert#'], .nav-link[href='../#js_alert#']");
   javascriptAlert.forEach(function (jsalert) {
     jsalert.addEventListener("click", function(event) {
+      event.preventDefault();
       alert("JavaScript is working!");
     });
   });
